15759ce96938432ac922338cc8ac87f169b6ca70
johnny <johnny@johnnys-iMac-Pro.local>
2023-06-08 17:21:22 -0300

    precss fields

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { useState } from 'react'
 import logo from './logo.svg';
 import poweredImage from './assets/powered.png'
 import styles from './App.module.css'
 
 function App() {
+const [heightField, setHeightField] = useState<number>(0) 
+const [weightField, setWeightField] = useState<number>(0)
+
+const handleCalcButton = () => {
+	if(heightField && weightField){
+	}else{
+	return alert("Preencha todos os campos")}
+}
   return (
 <div className={styles.main}>
 	<header>
@@ -12,7 +21,19 @@ function App() {
 	</div>
 	</header>
 	<div className={styles.container}>
-	<div className={styles.leftSide}>left</div>
+	<div className={styles.leftSide}><h1>Calcule o seu IMC</h1><p> IMC é a sigla para Indice de Massa Corpórea, parâmetro adotado pela Organização Mundial de Saúde para calcular o peso ideal de cada pessoa.</p>
+	<input type="number"
+	placeholder="Digite sua altura, em metros"
+	value={heightField > 0 ? heightField : ""} 
+	onChange={e => setHeightField(parseFloat(e.target.value))}  
+	/>
+<input type="number"
+	placeholder="Digite seu peso, em kilos"
+	value={weightField > 0 ? weightField : ""} 
+	onChange={e => setWeightField(parseFloat(e.target.value))}  
+	/>
+	<button onClick={handleCalcButton}>Calcular</button>
+	</div>
 	<div className={styles.rightSide}>right</div>
 </div>
 </div>
