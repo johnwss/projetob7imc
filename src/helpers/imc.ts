70d9a52de8c4c5cf72e13535a79ec33689e05425
johnny <johnny@johnnys-iMac-Pro.local>
2023-06-09 19:21:21 -0300

    helpers step

---
+++ src/helpers/imc.ts
@@ -0,0 +1,6 @@
+export const levels = [{title: 'Magreza', color: "blue", icon: "down", imc: [0,18.5]},
+	{title: "Normal", color: green, icon: "up", imc:[18.6, 24,9]},
+	{title: "Sobrepeso",color: yellow, icon: "down", imc:[25, 30] },
+	{title: "Öbesidade", color: red, icon: "down", imc: [30.1, 99]}
+]
+
