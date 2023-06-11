5a8c3bc2d686056e91f8c3bbed76a795d09b8d77
Johnny Santos <johnnysantos@MacBook-Air-de-Johnny.local>
2023-06-11 19:23:23 -0300

    firstlogic

--- src/helpers/imc.ts
+++ src/helpers/imc.ts
@@ -7,12 +7,16 @@ imc: number[]:
 
 
 export const levels: Level = [{title: 'Magreza', color: "blue", icon: "down", imc: [0,18.5]},
-	{title: "Normal", color: green, icon: "up", imc:[18.6, 24,9]},
-	{title: "Sobrepeso",color: yellow, icon: "down", imc:[25, 30] },
-	{title: "Öbesidade", color: red, icon: "down", imc: [30.1, 99]}
+	{title: "Normal", color: "green", icon: "up", imc:[18.6, 24,9]},
+	{title: "Sobrepeso",color: "yellow", icon: "down", imc:[25, 30] },
+	{title: "Öbesidade", color: "red", icon: "down", imc: [30.1, 99]}
 ]
 
 export const calculateImc = (height: number, weight: number) => {
-return
-} 
+const imc = weight /(height * height)
+for(let i in levels){
+if(imc >= levels[i].imc[0] && imc < levels[i].imc[1]){
+	return levels[i]
+}
+}
 
