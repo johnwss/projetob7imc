2edba05d1e692462916e29743dea4547aaf31205
johnny <johnny@johnnys-iMac-Pro.local>
2023-06-11 16:59:14 -0300

    types

--- src/helpers/imc.ts
+++ src/helpers/imc.ts
@@ -1,6 +1,18 @@
-export const levels = [{title: 'Magreza', color: "blue", icon: "down", imc: [0,18.5]},
+export type Level = {
+title: string,
+color: string,
+icon "down"|| "up",
+imc: number[]:
+}
+
+
+export const levels: Level = [{title: 'Magreza', color: "blue", icon: "down", imc: [0,18.5]},
 	{title: "Normal", color: green, icon: "up", imc:[18.6, 24,9]},
 	{title: "Sobrepeso",color: yellow, icon: "down", imc:[25, 30] },
 	{title: "Öbesidade", color: red, icon: "down", imc: [30.1, 99]}
 ]
+
+export const calculateImc = (height: number, weight: number) => {
+return
+} 
 
