f1cbaa629d11eefdf138022051b20f9098617ea8
johnny <johnny@johnnys-iMac-Pro.local>
2023-06-12 18:08:53 -0300

    starting right side

--- src/components/Griditens/Griditens.tsx
+++ src/components/Griditens/Griditens.tsx
@@ -1,11 +1,20 @@
-import { Level } from "../../helpers/imc"
+import styles from "./Griditens.module.css";
+import { Level } from "../../helpers/imc";
+import upImage from "../../assets/up.png";
+import downImage from "../../assets/down.png";
 
 type Props = {
-    itemData: Level
-}
+  itemData: Level;
+};
 
-export const Griditens = ({itemData}: Props) => {
-    return (
-        <div>{itemData.title}</div>
-    )
-}
+export const Griditens = ({ itemData }: Props) => {
+  return (
+    <div className={styles.main} style={{ backgroundColor: itemData.color }}>
+      <div className={styles.gridIcon}>
+        <img src={itemData.icon === "up" ? upImage : downImage} alt="" width="30" />
+      </div>
+      <div className={styles.gridTitle}>{itemData.title}</div>
+      <div className={styles.gridInfo}></div>
+    </div>
+  );
+};
